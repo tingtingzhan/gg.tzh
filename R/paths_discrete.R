f0f762ed55d38d13e42d4e2ff42c82ffef19ad53
Tingting Zhan <tingtingzhan@gmail.com>
2025-04-23 13:38:08 -0400

    tzh

---
+++ R/paths_discrete.R
@@ -0,0 +1,92 @@
+
+
+#' @title Bars of Discrete Function using \CRANpkg{ggplot2}
+#' 
+#' @description ..
+#' 
+#' @param fun a discrete \link[base]{function}
+#' 
+#' @param dots argument of \link[base]{function} `fun`
+#' 
+#' @param args ..
+#' 
+#' @param label ..
+#' 
+#' @param ... parameters of \link[geomtextpath]{geom_textpath}
+#' 
+#' @param xlim \link[base]{length} \eqn{\geq 2} \link[base]{integer} \link[base]{vector}, limit on \eqn{x}-axis.
+#' 
+#' @details ..
+#' 
+#' @returns
+#' Function [paths_discrete()] returns a \link[base]{list} of 
+#' \link[ggplot2]{layer}s.
+#' 
+#' @note 
+#' Be aware of potential name clash with function `ggpubr::ggbarplot`.
+#' 
+#' @examples 
+#' options(use_unicode = FALSE)
+#' 
+#' ggplot() + paths_discrete(dpois, dots = list(lambda = 1), xlim = 0:6)
+#' 
+#' ggplot() + paths_discrete(dnbinom, dots = list(size = 5.2, prob = c(.5, .4)), xlim = 0:15)
+#' 
+#' ggplot() + paths_discrete(dpois, dots = list(lambda = c(1, 1.3)), label = c('Treatment', 'Control'), 
+#'  xlim = 0:4) +
+#'  labs(x = 'Number of Re-Admissions', y = 'Conditional Probabilities', 
+#'   fill = 'Poisson\nDistribution',
+#'   caption = 'Illustration showing mean-ratio = .77 (Treatment vs. Control)')
+#'  
+#' @importFrom ggplot2 ggplot aes geom_point scale_x_continuous scale_y_continuous labs
+#' @importFrom geomtextpath geom_textpath
+#' @importFrom rlang .data
+#' @importFrom scales label_percent
+#' @export
+paths_discrete <- function(
+    fun, 
+    dots = list(),
+    args = split_list_(dots),
+    label = getval_(args),
+    xlim = stop('must provide `xlim` explicitly'),
+    ...
+) {
+  
+  if (!is.integer(xlim) || !length(xlim) || anyNA(xlim)) stop('`xlim` must be integer')
+  if (any(xlim < 0L)) stop('`xlim` must be non-negative')
+  
+  xs <- if (length(xlim) == 1L) {
+    if (xlim == 0L) stop('upper end of `xlim` needs to be positive')
+    0:xlim
+  } else min(xlim):max(xlim)
+  
+  ys <- args |>
+    lapply(FUN = \(i) do.call(what = fun, args = c(list(xs), i)))
+  
+  aes_d_ <- data.frame(
+    x = xs,
+    y = ys |> unlist(use.names = FALSE),
+    id = label |> seq_along() |> as.character() |> rep(each = length(xs)),
+    label = label |> rep(each = length(xs))
+  )
+  
+  mp <- if (length(args) > 1L) {
+    aes(x = .data$x, y = .data$y, color = .data$id, label = .data$label)
+  } else aes(x = .data$x, y = .data$y, label = .data$label)
+    
+  return(list(
+    
+    # (optional)
+    # geom_point(data = aes_d_, mapping = aes(x = .data$x, y = .data$y, color = .data$label), alpha = .5, ...),
+    
+    geom_textpath(data = aes_d_, mapping = mp, show.legend = FALSE, ...),
+    
+    scale_x_continuous(name = NULL, breaks = xs), # all (integer) `x` values printed
+      
+    scale_y_continuous(name = deparse1(substitute(fun)), labels = label_percent())
+    
+  ))
+  
+}
+
+
--- R/paths_function.R
+++ R/paths_function.R
@@ -32,51 +32,49 @@
 #' Default `'colour'`, i.e., parameters are discriminated using different colors. 
 #' Another good choice is `c('colour', 'linetype')`, i.e., parameters are discriminated using different colors and line-types.
 #' 
-#' @param ... argument of function \link[ggplot2]{geom_function}. 
+#' @param ... argument of function \link[ggplot2]{stat_function}. 
 #' Also see parameter `params` of function \link[ggplot2]{layer}
 #' 
 #' @returns 
-#' Function [geoms_function()] returns a \link[base]{list} of 
-#' \link[ggplot2]{geom_function}
+#' Function [paths_function()] returns a \link[base]{list} of 
 #' \link[ggplot2]{layer}s.
 #' 
 #' @examples 
 #' library(latex2exp)
 #' 
-#' ggplot() + geom_function(fun = dnorm, args = list(mean=0, sd=1)) + xlim(-3,3)
+#' ggplot() + stat_function(fun = dnorm, args = list(mean=0, sd=1)) + xlim(-3,3)
 #' 
-#' ggplot() + geoms_function(dnorm, dots = list(mean=1:0), 
+#' ggplot() + paths_function(dnorm, dots = list(mean=1:0), 
 #'   label = TeX(c('$mu_1=1$', '$mu_0=0$'))) + xlim(-3,4)
 #' 
-#' ggplot() + geoms_function(dnorm, dots = list(mean=0, sd=c(1,1.3)), n=501L, size=2) + xlim(-5,5)
-#' ggplot() + geoms_function(dnorm, dots=list(mean=0:1, sd=c(1,.9)), 
-#'   label = letters[1:2], aes_ = c('color', 'linetype')) + xlim(-5,5)
-#' ggplot() + geoms_function(dt, dots = list(df=c(1,2,5,Inf)), size = 2.5) + xlim(-4,4)
-#' ggplot() + geoms_function(dchisq, dots = list(df = c(1,3,5,10)), size = 2) + xlim(.2,8)
-#' ggplot() + geoms_function(dgamma, dots = list(shape = 1:4), size = 2) + xlim(0,5)
+#' ggplot() + paths_function(
+#'   fun = dnorm, dots = list(mean = 0, sd = c(1, 1.3)), 
+#'   aes_ = c('color', 'linetype'), size = 2
+#' ) + xlim(-5, 5)
+#' 
+#' ggplot() + paths_function(dt, dots = list(df=c(1,2,5,Inf)), size = 2.5) + xlim(-4,4)
+#' ggplot() + paths_function(dchisq, dots = list(df = c(1,3,5,10)), size = 2) + xlim(.2,8)
+#' ggplot() + paths_function(dgamma, dots = list(shape = 1:4), size = 2) + xlim(0,5)
 #' 
 #' # developer's use
-#' ggplot() + geoms_function(dnorm, args = list(
+#' ggplot() + paths_function(dnorm, args = list(
 #'  list(mean=0, sd=1), 
 #'  list(mean=1, sd=1.3)
 #' ), label = c('beta', 'alpha'), parse = TRUE) + xlim(-5,5)
 #' 
-#' # manually, without function [geoms_function()]
-#' library(geomtextpath)
+#' # manually, without function [paths_function()]
 #' ggplot() + 
 #'  stat_function(geom = 'textpath', fun = dnorm, args = list(mean=0, sd=1), 
-#'   mapping = aes(color = '1'), label = 'beta', 
-#'   parse = TRUE, show.legend = FALSE) +
+#'   mapping = aes(color = '1'), label = 'a', show.legend = FALSE) +
 #'  stat_function(geom = 'textpath', fun = dnorm, args = list(mean=1, sd=1.3), 
-#'   mapping = aes(color = '2'), label = 'alpha', 
-#'   parse = TRUE, show.legend = FALSE) +
+#'   mapping = aes(color = '2'), label = 'b', show.legend = FALSE) +
 #'  xlim(-5,5)
 #'
-#' @importFrom ggplot2 geom_function stat_function labs scale_colour_discrete scale_linetype_discrete
+#' @importFrom ggplot2 stat_function
 #' @importFrom geomtextpath geom_textpath
 #' @importFrom stats setNames
 #' @export
-geoms_function <- function(
+paths_function <- function(
     fun, 
     dots = list(),
     args = split_list_(dots),
@@ -90,11 +88,7 @@ geoms_function <- function(
   if (!is.function(fun)) stop('input must be \'function\'')
   
   if (missing(args)) {
-    if (!length(dots)) {
-      cl <- match.call()
-      cl[[1L]] <- quote(geom_function)
-      .Defunct(new = deparse1(cl))
-    }
+    if (!length(dots)) .Defunct(new = 'use ggplot2::stat_function')
     force(args) 
   }
   
@@ -105,7 +99,7 @@ geoms_function <- function(
     # do nothing
   } else sprintf(fmt = '%s `label` not supported', sQuote(class(label)[1L])) |> stop()
   
-  amap <- args |>
+  mp <- args |>
     seq_along() |>
     lapply(FUN = \(i) {
       i |> 
@@ -117,11 +111,14 @@ geoms_function <- function(
     })
   
   # return below
-  .mapply(FUN = stat_function, dots = list(
-      mapping = amap, 
+  .mapply(
+    FUN = stat_function, 
+    dots = list(
+      mapping = mp, 
       args = args,
       label = label |> as.character() # must for expression!
-    ), MoreArgs = list(
+    ), 
+    MoreArgs = list(
       fun = fun, 
       geom = 'textpath', # must Depends geomtextpath; Imports does not work. why??
       show.legend = FALSE, 
